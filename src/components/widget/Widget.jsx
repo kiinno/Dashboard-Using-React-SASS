4968fb52d8438944eedf912549266ac4e8c974ef
kiinno <clever01011@hotmail.com>
2023-02-26 01:26:47 +0200

    [ADD] Widget Components Finish

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,20 +1,93 @@
 import "./widget.scss";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import AccountBalanceOutlinedIcon from "@mui/icons-material/AccountBalanceOutlined";
+const Widget = ({ type }) => {
+  let data = {};
+  const amount = Math.floor(Math.random() * 2 ** 12);
+  const percentage = Math.floor(Math.random() * 101);
+  if (type) {
+    switch (type) {
+      case "order":
+        data = {
+          title: "orders",
+          isMoney: false,
+          link: "view all orders",
+          icon: (
+            <ShoppingCartOutlinedIcon
+              className="icon"
+              style={{
+                color: "goldenrod",
+                backgroundColor: "rgba(218,165,32,0.2)",
+              }}
+            />
+          ),
+        };
+        break;
+      case "earning":
+        data = {
+          title: "earnings",
+          isMoney: true,
+          link: "view net earnings",
+          icon: (
+            <MonetizationOnOutlinedIcon
+              className="icon"
+              style={{ color: "green", backgroundColor: "rgba(0,255,0,0.2)" }}
+            />
+          ),
+        };
+        break;
+      case "user":
+        data = {
+          title: "users",
+          isMoney: false,
+          link: "see all users",
+          icon: (
+            <PersonOutlineIcon
+              style={{ color: "crimson", backgroundColor: "rgba(255,0,0,0.2)" }}
+              className="icon"
+            />
+          ),
+        };
+        break;
+      case "balance":
+        data = {
+          title: "balance",
+          isMoney: false,
+          link: "see details",
+          icon: (
+            <AccountBalanceOutlinedIcon
+              className="icon"
+              style={{
+                color: "purple",
+                backgroundColor: "rgba(128,0,128,0.2)",
+              }}
+            />
+          ),
+        };
+        break;
+      default:
+        break;
+    }
+  }
 
-const Widget = ({ title, counter, percentage, icon }) => {
   return (
     <div className="widget">
       <div className="left">
-        <span className="title">{title}</span>
-        <span className="counter">{counter}</span>
-        <span className="link">see all {title}</span>
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"} {amount}
+        </span>
+        <span className="link">{data.link}</span>
       </div>
       <div className="right">
-        <div className="percentage positive">
+        <div className={`percentage`}>
           <ExpandLessIcon />
           {percentage}%
         </div>
-        {icon}
+        {data.icon}
       </div>
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,6 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
 import "./home.scss";
-import PeopleIcon from "@mui/icons-material/People";
-import LocalShippingIcon from "@mui/icons-material/LocalShipping";
-import ReportIcon from "@mui/icons-material/Report";
-import BorderColorIcon from "@mui/icons-material/BorderColor";
 
 const Home = () => {
   return (
@@ -14,30 +10,10 @@ const Home = () => {
       <div className="homeContainer">
         <Navbar />
         <div className="widgets">
-          <Widget
-            title={"users"}
-            counter={24134}
-            percentage={55}
-            icon={<PeopleIcon />}
-          />
-          <Widget
-            title={"orders"}
-            counter={443}
-            percentage={32}
-            icon={<BorderColorIcon />}
-          />
-          <Widget
-            title={"reports"}
-            counter={44}
-            percentage={12}
-            icon={<ReportIcon />}
-          />
-          <Widget
-            title={"delivering"}
-            counter={7654}
-            percentage={65}
-            icon={<LocalShippingIcon />}
-          />
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
         </div>
       </div>
     </div>
